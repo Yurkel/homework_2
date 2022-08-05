d8015c582e2bc9aa9825118328556df16a7faa02
Yuriy <yuriy-k@ukr.net>
2022-08-05 11:11:26 +0300

    add homework_1pt2

---
+++ main.js
@@ -0,0 +1,5 @@
+const firstNumber = prompt("Enter the first number");
+const secondNumber = prompt("Enter the second number");
+let finalNumber = +firstNumber + +secondNumber;
+
+alert(finalNumber);
